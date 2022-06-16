700427e5b69856eddce81c35dda287f6aedb2870
Luke Hoang <61900428+lukehoang1905@users.noreply.github.com>
2022-06-16 15:32:50 +0700

    Revert "Pokedex-be, Pokedex-fe"

--- module-3/pokedex-fe/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import pokemonReducers from "../features/pokemons/pokemonSlice";
-const store = configureStore({
-    reducer: {
-        pokemons: pokemonReducers
-    }
-});
-
-export default store;
